7460a8bb5948b8274d532ef1f412f16597582b41
Umang <soyumang55@gmail.com>
2024-12-17 20:31:51 +0530

    conversation with bot created

--- smollm.py
+++ smollm.py
@@ -5,7 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 template = """
-Answer the question below
+Answer the question below with only what is asked and do not provide any other extra text aside from the question asked
 
 Here is the conversation history: {context}
 
@@ -26,7 +26,7 @@ def stop_ollama_server(process):
     process.terminate()
     process.wait()
     
-def invoke_model(input_text):
+def invoke_model():
     ollama_process = start_ollama_server()
     
     # Allow some time for the server to initialize
@@ -35,13 +35,27 @@ def invoke_model(input_text):
     model = OllamaLLM(model="smollm")
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | model
-    result = chain.invoke({"context":"", "question":input_text})       
+    
+    final_context=handle_conversation(chain)
+    result = chain.invoke({"context":final_context, "question":""})       
     # (input=input_text)
     
     stop_ollama_server(ollama_process)
     return result
 
-input_text = "Hello world"
-result = invoke_model(input_text)
+def handle_conversation(model):
+    context=""
+    print("WECOME to SystemLLM! Type 'exit' to quit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+
+        result = model.invoke({"context":context, "question":user_input})
+        print("Bot: ", result)
+        context+= f"\nUser: {user_input}\nAI: {result}"
+    return context
+        
+result = invoke_model()
 
 print(result)
