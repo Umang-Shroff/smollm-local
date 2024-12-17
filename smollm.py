fa77b50d5d650521a0d8418367cb6a7289e7a836
Umang <soyumang55@gmail.com>
2024-12-17 21:38:55 +0530

    file retrieval llm setup

--- smollm.py
+++ smollm.py
@@ -2,6 +2,14 @@ import subprocess
 import time
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.indexes import VectorstoreIndexCreator
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+# from langchain_community.embeddings import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
+
+
+import os
 
 
 template = """
@@ -30,30 +38,50 @@ def stop_ollama_server(process):
     process.terminate()
     process.wait()
     
+def load_data():
+    """Load data from `data.txt` file"""
+    loader = TextLoader("data.txt")
+    embeddings = OllamaEmbeddings(model="smollm")
+    persist_directory = "persist"
+    vectorstore = Chroma.from_documents(loader.load(), embeddings, persist_directory=persist_directory)
+    return vectorstore
+    
+def retrieve_context(query, vectorstore):
+    """Retrieve relevant context from the data based on users question."""
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
+    result = retriever.get_relevant_documents(query)
+    return result[0].page_content if result else ""
+    
 def invoke_model():
     ollama_process = start_ollama_server()
     
     # Allow some time for the server to initialize
     time.sleep(5)
     
+    vectorstore = load_data()
+    
     model = OllamaLLM(model="smollm")
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | model
     
-    final_context=handle_conversation(chain)
+    final_context=handle_conversation(chain, vectorstore)
     # result = chain.invoke({"context":final_context, "question":""})       
     # (input=input_text)
     
     stop_ollama_server(ollama_process)
     # return result
 
-def handle_conversation(model):
+def handle_conversation(model, vectorstore):
     context=""
     print("\nWECOME to SystemLLM! Type 'exit' to quit.\n")
     while True:
         user_input = input("You: ")
         if user_input.lower() == "exit":
             break
+        
+        relevant_context = retrieve_context(user_input, vectorstore)
+        
+        context += f"\nUser: {user_input}\nAI: {relevant_context}"
 
         result = model.invoke({"context":context, "question":user_input})
         print("Bot: ", result)
