1de8345c88b3c2c438fcf3052eb9c597ae653c21
Umang <soyumang55@gmail.com>
2024-12-17 19:58:16 +0530

    start_server_function

--- smollm.py
+++ smollm.py
@@ -1,3 +1,10 @@
+import subprocess
+import time
 from langchain_ollama import OllamaLLM
 
 model = OllamaLLM(model="smollm")
+
+def start_ollama_server():
+    # Start the Ollama server using subprocess
+    print("Starting Ollama server...")
+    return subprocess.Popen(["ollama", "serve"])
