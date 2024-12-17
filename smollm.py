647f24591935bb834713c200e5017fa9ffc37999
Umang <soyumang55@gmail.com>
2024-12-17 20:04:02 +0530

    template initialized

--- smollm.py
+++ smollm.py
@@ -1,6 +1,19 @@
 import subprocess
 import time
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+
+template = """
+Answer the question below
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer: 
+""" 
+
 
 def start_ollama_server():
     # Start the Ollama server
